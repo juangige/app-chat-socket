48ff5c9ba8042bf2ca05d615d7d8b15e40e241b0
Jotita <146959435+juangige@users.noreply.github.com>
2024-06-03 11:51:36 -0300

    Primer commit

---
+++ public/js/main.js
@@ -0,0 +1,60 @@
+console.log("Conectando...")
+
+const socket = io();
+
+let user;
+const chatBox = document.getElementById("chatBox");
+
+Swal.fire({
+    title: "Bienvenido",
+    text: "Escribe tu nombre de usuario",
+    input: "text",
+    inputValidator: (value) => {
+         !value && ("Debes escribir un nombre de usuario");
+
+        return value.trim() === "" && "El nombre de usuario no puede estar vacío";
+    },
+    allowOutsideClick: false,
+    allowEscapeKey: false,
+    allowEnterKey: false,
+    stopKeydownPropagation: false
+  }).then((result) => {
+    user = result.value;
+    console.log("Usuario:", user);
+  })
+
+  chatBox.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        socket.emit("message", {
+            user,
+            message: chatBox.value,
+            date: new Date().getTime()
+        })
+        console.log("Enviando:", chatBox.value);
+        chatBox.value = "";
+    }
+  })
+
+  socket.on("connected", (data) => {
+    if(user !== data) {
+        Swal.fire({
+            text: `${data} se ha unido al chat`,
+            toast: true,
+            position: "top-right"
+        });
+    }
+});
+
+// manejo de los mensajes
+socket.on("messages", (data) => {
+    console.log("Recibiendo:", data);
+    const messagesContent = document.getElementById("messages");
+    let content = "";
+
+    data.forEach((messages) => {
+        content += `<b>${messages.user}</b>: ${messages.message} <br/>`;
+    });
+    
+    messagesContent.innerHTML = content;
+    
+});
---
+++ src/server.js
@@ -0,0 +1,50 @@
+import express from "express";
+import { Server } from "socket.io";
+import __dirname from "./dirname.js";
+import path from "path";
+
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.resolve(__dirname, "../public")));
+
+
+const httpServer = app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto http://localhost:${PORT}`);
+});
+
+//Socket IO
+const io = new Server(httpServer);
+
+const messages = [];
+
+io.on("connection", (socket) => {
+    console.log("Un usuario se ha conectado", socket.id);
+
+    // manejo de los mensajes
+    socket.on("message", (data) => {
+        messages.push(data);
+        io.emit("messages", messages);
+    });
+
+    // manejo cuando se conecta un nuevo usuario
+    socket.on("inicio", (data) => {
+        io.emit("inicio", data);
+        socket.broadcast.emit("connected", data);
+    })
+
+    socket.emit("messages", messages);
+
+    socket.on("disconnect", () => {
+        console.log("Un usuario se ha desconectado", socket.id);
+
+    socket.on("disconnected", (data) => {
+        io.emit("disconnected", data);
+        socket.broadcast.emit("disconnected", data);
+    })
+
+    });
+
+});
